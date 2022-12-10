b82684f0e70eaf9cdbcd1f34c58de83e65927480
Aantic96 <gemory.mei@gmail.com>
2022-12-10 23:18:56 +0100

    Small change in condition.

--- mod4_solution/harder/script.js
+++ mod4_solution/harder/script.js
@@ -3,7 +3,7 @@
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (i = 0; i < names.length; i++) {
-  if (names[i][0] == "J") {
+  if (names[i][0] == "J" || names[i][0] == "j") {
     byeSpeaker.speak(names[i]);
   } else {
     helloSpeaker.speak(names[i]);
